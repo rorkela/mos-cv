6f087835187082b6f5d3a37366e970fc39aef75a
Naman-8bit <naman30062006@gmail.com>
2025-09-21 12:56:25 +0000

    fixed the code

--- poisson/poisson.c
+++ poisson/poisson.c
@@ -102,22 +102,22 @@ void thomas(double* A,double* B,int N,double* x){
     // B matrix should be Nx1
     // X is the update so Nx1 
 
-    // pass the number of rows in this function which is just mos,nz
-    double c_new[rows];//the diagonal elements above the one wala in the T matrix
-    double d_new[rows];//the B matrix after the row operations and normalisation
+    // pass the number of rows in this function which is just mos.nz
+    double c_new[N];//the diagonal elements above the one wala in the T matrix
+    double d_new[N];//the B matrix after the row operations and normalisation
     // Forward elimination
     // normalise the first and put the values already
     c_new[0] = A[2] / A[1];          // c1 / b1
     d_new[0] = B[0] / A[1];          // d1 / b1
 
-    for (int i = 1; i < rows; i++) {
+    for (int i = 1; i < N; i++) {
         double denominator = A[i*3 + 1] - A[i*3 + 0] * c_new[i - 1];  
-        c_new[i] = (i == rows-1) ? 0.0 : A[i*3 + 2] / denominator;   
+        c_new[i] = (i == N-1) ? 0.0 : A[i*3 + 2] / denominator;   
         d_new[i] = (B[i] - A[i*3 + 0] * d_new[i - 1]) / denominator;
     }
     // Back substitution
-    x[rows - 1] = d_new[rows - 1];
-    for (int i = rows - 2; i >= 0; i--) {
+    x[N - 1] = d_new[N - 1];
+    for (int i = N - 2; i >= 0; i--) {
         x[i] = d_new[i] - c_new[i] * x[i + 1];
     }    
 }
